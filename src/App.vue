3fa19c16c63db8e915c836da03d45bc836e27e9d
giovannitamburini <giovatambu@gmail.com>
2023-03-30 21:48:16 +0200

    added API call & store

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <script>
+
+// importo axios per le API call
+import axios from "axios";
+// importo lo store per la condivisione di dati
 import { store } from "./store.js";
 
 export default {
@@ -6,7 +10,19 @@ export default {
     return {
       store,
     }
-  }
+  },
+
+  created() {
+
+    axios.get(this.store.ApiCall).then((res) => {
+
+      // eguaglio l'array ListMovies, dello store, all array che otteniamo dalla API call
+      this.store.ListMovies = res.data.results;
+
+      // esempio
+      console.log(this.store.ListMovies);
+    })
+  },
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -2,4 +2,7 @@ import { reactive } from 'vue';
 
 export const store = reactive({
 
+    ApiCall: 'https://api.themoviedb.org/3/search/movie?api_key=c3c804e9852eacc93d32f48f228a0f79&language=it-IT&query=signore anelli',
+
+    ListMovies: [],
 });
